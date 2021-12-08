9b2ec3aa7afa5e9914c051b3abd5d5e8ab2cc041
deppong <30512867+deppong@users.noreply.github.com>
2021-12-08 17:29:02 -0500

    initial commit, cmake working

---
+++ src/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <SDL.h>
+
+#define WIDTH 800
+#define HEIGHT 800
+
+int main(int argc, int* argv[]) {
+    int quit = 0;
+    SDL_Event event;
+
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window *window = SDL_CreateWindow("Lorenz", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    while(!quit) {
+        SDL_WaitEvent(&event);
+
+        switch (event.type) {
+            case SDL_QUIT: quit = 1;break;
+            case SDL_KEYDOWN: if (event.key.keysym.sym == SDLK_ESCAPE) quit=1;break;
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+    return 0;
+}
