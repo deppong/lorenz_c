cecfdf401c060273b6743cc73509806ff9e6da4f
deppong <30512867+deppong@users.noreply.github.com>
2021-12-08 19:35:53 -0500

    outputting properly, added user input functionality

--- src/main.c
+++ src/main.c
@@ -1,23 +1,62 @@
 #include <stdio.h>
 #include <SDL.h>
 
-#define WIDTH 800
-#define HEIGHT 800
+#define WIDTH 1000
+#define HEIGHT 1000
 
 int main(int argc, int* argv[]) {
     int quit = 0;
     SDL_Event event;
 
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("Lorenz", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
+    SDL_Window *window = SDL_CreateWindow("Lorenz Attractor", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    while(!quit) {
-        SDL_WaitEvent(&event);
+    SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
 
+    // Lorenz equations initialization
+    // https://en.wikipedia.org/wiki/Lorenz_system
+
+    double x=0.01,y=0.01,z=0.0;
+    double rho   = 28.0;
+    double sigma = 10.0;
+    double beta  = 8.0/3.0;  
+    double dt = 0.01;
+
+    int counter = 0, step = 1, scale = 8;
+
+    while(!quit) {
+        
+        SDL_PollEvent(&event);
         switch (event.type) {
             case SDL_QUIT: quit = 1;break;
-            case SDL_KEYDOWN: if (event.key.keysym.sym == SDLK_ESCAPE) quit=1;break;
+            case SDL_KEYDOWN: 
+                switch (event.key.keysym.sym) {
+                    case SDLK_ESCAPE: quit=1;break;
+                    // pause simulation
+                    case SDLK_SPACE: step=step?0:1;break;
+                    // clear screen
+                    case SDLK_c: 
+                        SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+                        SDL_RenderClear(renderer);
+                        break;
+                }
+        }
+
+        if (step) {
+            double dx = (sigma * (y - x)) * dt;
+            double dy = (x * (rho - z) - y) * dt;
+            double dz = (x*y - beta*z) * dt;
+
+            x = x + dx;
+            y = y + dy;
+            z = z + dz;
+            SDL_SetRenderDrawColor(renderer, 30 + counter, 0 + counter, 40 + counter, 255);
+            SDL_RenderDrawPoint(renderer, x*scale + WIDTH/2, y*scale + HEIGHT/2);
+            SDL_RenderPresent(renderer);
+            counter+=1;
         }
     }
 
